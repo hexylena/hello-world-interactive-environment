058c0f109adb6717464f5232a36b8f9c3ccca7a1
Eric Rasche <rasche.eric@gmail.com>
2015-07-10 13:53:57 -0500

    Better docs on startup

--- startup.sh
+++ startup.sh
@@ -3,11 +3,13 @@
 sed -i "s| '\*'; # IE_CORS_ORIGIN| '${CORS_ORIGIN}';|" /proxy.conf;
 cp /proxy.conf /etc/nginx/conf.d/default.conf;
 
-# Correct permissions on the folder
-chown $uid:$gid /import -R
-
+# Here you would normally start whatever service you want to start. In our
+# example we start a simple directory listing service on port 8000
 cd /import/ && python -mSimpleHTTPServer &
-# Launch traffic monitor
+
+
+# Launch traffic monitor which will automatically kill the container if traffic
+# stops
 /monitor_traffic.sh &
 # And nginx in foreground mode.
 nginx -g 'daemon off;'
